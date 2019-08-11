8e64c0d9d91b391a018582bba21033af616c57f6
Francisco Guiraldelli <francisco.guiraldelli.com>
2019-08-11 14:14:56 -0300

    Exercise to create services in the joke app.

---
+++ src/main/java/guru/springframework/joke/services/JokeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.joke.services;
+
+/**
+ * Created by fguira on 2019-08-11 at 14:06
+ */
+public interface JokeService {
+
+    String getJoke();
+
+}
---
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -0,0 +1,24 @@
+package guru.springframework.joke.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by fguira on 2019-08-11 at 14:07
+ */
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
