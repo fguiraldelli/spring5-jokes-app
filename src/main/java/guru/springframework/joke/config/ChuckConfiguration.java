76b9a73ee366a25d212038d544c5ab4cff0b7e7d
Francisco Guiraldelli <francisco.guiraldelli.com>
2019-08-12 00:27:45 -0300

    Exercise from custom-banner to java-config-example

---
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package guru.springframework.joke.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by fguira on 2019-08-12 at 00:12
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
